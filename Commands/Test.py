8ed38b9b260f8fd645c95c14908dceda22ddc917
ItsssJustice <magiful@googlemail.com>
2026-08-31 12:30:30 +0100

    Commands/Test.py - Deleted; other functional command examples exist now

--- Commands/Test.py
+++
@@ -1,7 +0,0 @@
-@tree.command(
-    name="test",
-    description="Test Command",
-    guild=discord.Object(id=DISCORD_GUILD)
-)
-async def first_command(interaction):
-    await interaction.response.send_message("Hello!")
